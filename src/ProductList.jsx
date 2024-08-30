9a00747450d18da50f4053bb88d7eb5962ae3d9b
Laila <lailamohammadabdulaziz@gmail.com>
2024-08-30 23:34:16 +0300

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import './ProductList.css';
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'; // Import useSelector
 
 function ProductList() {
-  const [showCart, setShowCart] = useState(false); 
+  const [showCart, setShowCart] = useState(false);
   const dispatch = useDispatch();
 
- const plantsArray = [
+  // Use useSelector to get the total quantity of items in the cart
+  const totalQuantity = useSelector((state) => state.cart.items.reduce((total, item) => total + item.quantity, 0));
+
+  const plantsArray = [
     {
         category: "Air Purifying Plants",
         plants: [
@@ -274,7 +277,20 @@ function ProductList() {
         </div>
         <div style={styleObjUl}>
           <div> <a href="#" onClick={handlePlantsClick} style={styleA}>Plants</a></div>
-          <div> <a href="#" onClick={handleCartClick} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+          <div>
+            <a href="#" onClick={handleCartClick} style={styleA}>
+              <h1 className='cart'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                  <rect width="156" height="156" fill="none"></rect>
+                  <circle cx="80" cy="216" r="12"></circle>
+                  <circle cx="184" cy="216" r="12"></circle>
+                  <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                </svg>
+                {/* Display the total quantity next to the cart icon */}
+                <span style={{ color: 'white', fontSize: '20px', marginLeft: '10px' }}>{totalQuantity}</span>
+              </h1>
+            </a>
+          </div>
         </div>
       </div>
       {!showCart ? (
@@ -304,4 +320,5 @@ function ProductList() {
 }
 
 export default ProductList;
+
 
